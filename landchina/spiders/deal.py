7900fc40b1e48b50989e2c00f0577202cf59d7bb
Steve Sun <sund.chn@gmail.com>
2017-01-17 23:37:27 +0800

    updated

--- landchina/spiders/deal.py
+++ landchina/spiders/deal.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 import datetime
+import time
+import re
 
 from selenium import webdriver
 from scrapy.spiders import Spider
@@ -11,21 +13,33 @@ from landchina.settings import BASE_URL, PROVINCE_BASE, PROVINCE_MAP, CELL_MAP
 
 
 class BreakPointTrack(object):
+    last_time = time.time()
 
     def __init__(self, url, page_no):
         self.url = url
         self.page_no = page_no
         self.save()
 
+    @classmethod
+    def get_last(cls):
+        return cls.last_time
+
+    @classmethod
+    def set_last(cls, now):
+        cls.last_time = now
+
     def save(self):
-        track = "{time} - [url:{url}] - [page:{page}]\n".format(
+        now = time.time()
+        track = "{time} - [url:{url}] - [page:{page}] - [cost:{cost}]\n".format(
             time=datetime.datetime.now(),
             url=self.url,
             page=self.page_no,
+            cost= now - self.get_last()
         )
         with open("breakpoint.log", "a") as f:
             f.write(track)
 
+        self.set_last(now)
 
 class Province(object):
 
@@ -79,14 +93,10 @@ class Mapper(object):
         for prvn in self.iterprvn():
             for url in self.iterurl(prvn):
                 page = Page(url, self.driver)
-                page_ok = True
-                while page and page_ok:
-                    BreakPointTrack(url, page.page_no)
+                while page:
                     for cellurl in page.fetchall():
-                        if not cellurl:
-                            page_ok = False
-                            break
                         yield cellurl
+                    BreakPointTrack(url, page.page_no)
                     page = page.go_to_next()
 
     def iterreq(self):
@@ -104,25 +114,15 @@ class Page(object):
         self.driver = driver
         if self.page_no == 1:
             self.driver.get(self.url)
-
-        self.next_btn = self.get_next_btn()
+        paper = self.driver.find_element_by_class_name('pager')
+        self.page_max = int(re.search(r'[0-9]\d*', paper.text).group(0))
 
     def go_to_next(self):
-        if not self.next_btn:
+        if self.page_no >= self.page_max:
             return None
-
-        self.next_btn.click()
-        new_page = Page(self.url, self.driver, self.page_no+1)
-        return new_page
-
-    def get_next_btn(self):
-        try:
-            btn = self.driver.find_element_by_link_text(u'下页')
-            if btn.get_attribute('disabled'):
-                return None
-            return btn
-        except NoSuchElementException:
-            return None
+        self.driver.execute_script("document.getElementById('TAB_QuerySubmitPagerData').setAttribute('value', %s)" % (self.page_no+1))
+        self.driver.execute_script("document.getElementById('mainForm').submit()")
+        return Page(self.url, self.driver, self.page_no+1)
 
     def fetchall(self):
         try:
@@ -145,6 +145,7 @@ class LandDealSpider(Spider):
     def __init__(self, name=None, **kwargs):
         service_args = ['--load-images=false', '--disk-cache=true']
         self.driver = webdriver.PhantomJS(service_args=service_args)
+        self.driver.implicitly_wait(10)
         super(LandDealSpider, self).__init__(name, **kwargs)
 
     def close(self, reason):
